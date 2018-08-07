80bc606e3f2ac6cb59fb532bf5f7964cf5b674d1
Yichao Yu <yyc1992@gmail.com>
2018-08-06 21:55:32 -0400

    [lib] pulses

--- lib/controller.h
+++ lib/controller.h
@@ -48,7 +48,13 @@ class Controller : public CtrlIFace {
             Hold = 1 << 7,
             Init = 1 << 8,
             // Control
-            TimeCheck = 0x8000000
+            DDS = 0x10000000,
+            Wait = 0x20000000,
+            ClearErr = 0x30000000,
+            LoopBack = 0x40000000,
+            ClockOut = 0x50000000,
+            SPI = 0x60000000,
+            TimeCheck = 0x8000000,
         };
     };
 
@@ -113,14 +119,132 @@ class Controller : public CtrlIFace {
         write(3, r3 | Bits::Init);
         write(3, r3 & ~Bits::Init);
     }
+
+    // Pulses
+    template<bool checked>
     inline void short_pulse(uint32_t ctrl, uint32_t op)
     {
+        if (checked)
+            ctrl = ctrl | Bits::TimeCheck;
         write(31, op);
         write(31, ctrl);
     }
-    inline void checked_pulse(uint32_t ctrl, uint32_t op)
+    template<bool checked>
+    inline void ttl_pulse(uint32_t ttl, uint32_t t)
     {
-        short_pulse(ctrl | Bits::TimeCheck, op);
+        assert(t < (1 << 24));
+        short_pulse<checked>(t, ttl);
+    }
+    template<bool checked>
+    inline void clock_pulse(uint8_t div)
+    {
+        short_pulse<checked>(Bits::ClockOut, div & 0xff);
+    }
+    template<bool checked>
+    inline void spi_pulse(uint8_t clk_div, uint8_t spi_id, uint32_t data)
+    {
+        uint32_t opcode = ((uint32_t(spi_id & 3) << 11) | clk_div);
+        short_pulse<checked>(opcode | Bits::SPI, data);
+    }
+    template<bool checked>
+    inline void dac_pulse(uint8_t dac, uint16_t V)
+    {
+        spi_pulse<checked>(0, 0, ((dac & 3) << 16) | V);
+    }
+    template<bool checked>
+    inline void dds_pulse(uint32_t ctrl, uint32_t op)
+    {
+        short_pulse<checked>(Bits::DDS | ctrl, op);
+    }
+    // set bytes at addr + 1 and addr
+    template<bool checked>
+    inline void dds_set_2bytes_pulse(int i, uint32_t addr, uint32_t data)
+    {
+        // put addr in bits 15...9 (maps to DDS opcode_reg[14:9])?
+        // put data in bits 15...0 (maps to DDS operand_reg[15:0])?
+        dds_pulse<checked>(0x2 | (i << 4) | (((addr + 1) & 0x7f) << 9), data & 0xffff);
+    }
+    // set bytes addr + 3 ... addr
+    template<bool checked>
+    inline void dds_set_4bytes_pulse(int i, uint32_t addr, uint32_t data)
+    {
+        // put addr in bits 15...9 (maps to DDS opcode_reg[14:9])?
+        dds_pulse<checked>(0xf | (i << 4) | (((addr + 1) & 0x7f) << 9), data);
+    }
+    template<bool checked>
+    inline void wait_pulse(uint32_t t)
+    {
+        assert(t < (1 << 24));
+        short_pulse<checked>(Bits::Wait | t, 0);
+    }
+    // clear timing check (clear failures)
+    template<bool checked>
+    inline void clear_error_pulse()
+    {
+        short_pulse<checked>(Bits::ClearErr, 0);
+    }
+    template<bool checked>
+    inline void dds_set_freq_pulse(int i, uint32_t ftw)
+    {
+        return dds_pulse<checked>(i << 4, ftw);
+    }
+    template<bool checked>
+    inline void dds_set_amp_pulse(int i, uint16_t amp)
+    {
+        return dds_set_2bytes_pulse<checked>(i, 0x32, amp);
+    }
+    template<bool checked>
+    inline void dds_set_phase_pulse(int i, uint16_t phase)
+    {
+        return dds_set_2bytes_pulse<checked>(i, 0x30, phase);
+    }
+    template<bool checked>
+    inline void dds_reset_pulse(int i)
+    {
+        return dds_pulse<checked>(0x4 | (i << 4), 0);
+    }
+
+    // Pulses with results
+    // clear timing check (clear failures)
+    template<bool checked>
+    inline void loopback_pulse(uint32_t data)
+    {
+        short_pulse<checked>(Bits::LoopBack, data);
+    }
+    template<bool checked>
+    inline void dds_get_2bytes_pulse(int i, uint32_t addr)
+    {
+        dds_pulse<checked>(0x3 | (i << 4) | ((addr + 1) << 9), 0);
+    }
+    template<bool checked>
+    inline void dds_get_4bytes_pulse(int i, uint32_t addr)
+    {
+        dds_pulse<checked>(0xe | (i << 4) | ((addr + 1) << 9), 0);
+    }
+    template<bool checked>
+    inline void dds_get_phase_pulse(int i)
+    {
+        dds_get_2bytes_pulse<checked>(i, 0x30);
+    }
+    template<bool checked>
+    inline void dds_get_amp_pulse(int i)
+    {
+        dds_get_2bytes_pulse<checked>(i, 0x32);
+    }
+    template<bool checked>
+    inline void dds_get_freq_pulse(int i)
+    {
+        dds_get_4bytes_pulse<checked>(i, 0x2c);
+    }
+
+    // Composite pulses
+    template<bool checked=false>
+    inline void dds_exists_pulse(int i)
+    {
+        dds_set_2bytes_pulse<checked>(i, 0x68, 0);
+        dds_get_2bytes_pulse<checked>(i, 0x68);
+        dds_set_2bytes_pulse<checked>(i, 0x68, 1);
+        dds_get_2bytes_pulse<checked>(i, 0x68);
     }
 public:
     Controller();
