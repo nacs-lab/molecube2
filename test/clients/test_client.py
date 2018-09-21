8d4eddd7a60ac9836457832e4829e4298d7a5d12
Yichao Yu <yyc1992@gmail.com>
2018-09-20 22:17:25 -0400

    [test/clients] dds override commands

--- test/clients/test_client.py
+++ test/clients/test_client.py
@@ -13,6 +13,34 @@ sock.connect(sys.argv[1])
 
 cmd = sys.argv[2]
 
+def parse_ddschn(chn):
+    if chn.startswith('freq'):
+        typ = 0
+        chn = chn[4:]
+    elif chn.startswith('amp'):
+        typ = 1
+        chn = chn[3:]
+    elif chn.startswith('phase'):
+        typ = 2
+        chn = chn[5:]
+    else:
+        raise ValueError("Invalid DDS channel name: " + chn)
+    chn = int(chn)
+    return typ << 6 | chn
+
+def print_ddschn(chn):
+    typ = chn >> 6
+    chn = chn & ((1 << 6) - 1)
+    if typ == 0:
+        name = 'freq'
+    elif typ == 1:
+        name = 'amp'
+    elif typ == 2:
+        name = 'phase'
+    else:
+        raise ValueError("Invalid DDS channel type: %d" % typ);
+    return "%s(%d)" % (name, chn)
+
 if cmd == 'set_startup':
     with open(sys.argv[3]) as fh:
         cmdlist = fh.read().encode()
@@ -31,7 +59,6 @@ elif cmd == 'set_ttl_names':
     for i in range(3, len(sys.argv), 2):
         msg += int(sys.argv[i]).to_bytes(1, byteorder=sys.byteorder,
                                          signed=False) + sys.argv[i + 1].encode() + b'\0'
-    print(msg)
     sock.send(msg)
     msg = sock.recv()
     print(msg)
@@ -45,7 +72,6 @@ elif cmd == 'set_dds_names':
     for i in range(3, len(sys.argv), 2):
         msg += int(sys.argv[i]).to_bytes(1, byteorder=sys.byteorder,
                                          signed=False) + sys.argv[i + 1].encode() + b'\0'
-    print(msg)
     sock.send(msg)
     msg = sock.recv()
     print(msg)
@@ -74,3 +100,21 @@ elif cmd == 'set_clock':
 elif cmd == 'get_clock':
     sock.send_string("get_clock")
     print("clock: {0}".format(struct.unpack('B', sock.recv())[0]))
+elif cmd == 'override_dds':
+    sock.send_string("override_dds", zmq.SNDMORE)
+    msg = b''
+    for i in range(3, len(sys.argv), 2):
+        chn = parse_ddschn(sys.argv[i]).to_bytes(1, byteorder=sys.byteorder, signed=False)
+        val = int(sys.argv[i + 1], 16).to_bytes(4, byteorder=sys.byteorder, signed=False)
+        msg += chn + val
+    sock.send(msg)
+    msg = sock.recv()
+    print(msg)
+elif cmd == 'get_override_dds':
+    sock.send_string("get_override_dds")
+    msg = sock.recv()
+    assert len(msg) % 5 == 0
+    print("%d DDS overrides" % (len(msg) / 5))
+    for i in range(0, len(msg), 5):
+        print("  {0} = {1:#0{2}x}".format(print_ddschn(msg[i]),
+                                           struct.unpack('I', msg[i + 1:i + 5])[0], 10))
