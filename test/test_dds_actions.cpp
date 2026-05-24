8971a794b9b4815341e22d38c765ceb2266d54e8
Yichao Yu <yyc1992@gmail.com>
2026-05-24 11:36:14 -0400

    Test DDS write and reset

---
+++ test/test_dds_actions.cpp
@@ -0,0 +1,54 @@
+#include "../lib/pulser.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+
+void reset_dds(Molecube::Pulser &p, int i)
+{
+    p.dump_dds(std::cout, i);
+    p.dds_reset<true>(i);
+    while (!p.is_finished()) {
+    }
+    p.dump_dds(std::cout, i);
+}
+
+void write_dds(Molecube::Pulser &p, int i, int type)
+{
+    p.dump_dds(std::cout, i);
+    for (int addr = 0x34; addr < 0x6c; addr += 2) {
+        if (addr == 0x64 || addr == 0x66)
+            continue;
+        dds_set_2bytes<false>(i, addr, (addr + type) | (addr << 8));
+    }
+    while (!p.is_finished()) {
+    }
+    p.dump_dds(std::cout, i);
+}
+
+int main(int argc, char **argv)
+{
+    auto addr = Molecube::Pulser::address();
+    if (!addr) {
+        printf("Cannot find pulser\n");
+        return 1;
+    }
+    Molecube::Pulser p(addr);
+    if (argc != 3) {
+        printf("Require two arguments: test_dds_actions action chn\n");
+        return 1;
+    }
+    if (strcmp(argv[1], "reset") == 0) {
+        reset_dds(p, atoi(argv[2]));
+    }
+    else if (strcmp(argv[1], "write1") == 0) {
+        write_dds(p, atoi(argv[2]), 1);
+    }
+    else if (strcmp(argv[1], "write2") == 0) {
+        write_dds(p, atoi(argv[2]), 2);
+    }
+
+    return 0;
+}
