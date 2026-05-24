ecb951e354a26a26e98ba4ef92f86531ee2e70e2
Yichao Yu <yyc1992@gmail.com>
2026-05-24 11:59:53 -0400

    Add output test

--- test/test_dds_actions.cpp
+++ test/test_dds_actions.cpp
@@ -3,8 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include <iostream>
+
+static constexpr double pow2_32 = 1. / pow(2, 32);
+
+static inline constexpr uint32_t
+freq2num(double f, double clock = 3.5e9)
+{
+    return static_cast<uint32_t>(0.5 + f / clock * (1 / pow2_32));
+}
+
+static inline constexpr double
+num2freq(uint32_t num, double clock = 3.5e9)
+{
+    return num * clock * pow2_32;
+}
+
+static inline constexpr uint16_t
+amp2num(double amp)
+{
+    return 0x0fff & static_cast<uint16_t>(amp * 4095.0 + 0.5);
+}
+
+static inline constexpr double
+num2amp(uint32_t num)
+{
+    return num / 4095.0;
+}
 
 void reset_dds(Molecube::Pulser &p, int i)
 {
@@ -23,6 +50,16 @@ void write_dds(Molecube::Pulser &p, int i, int type)
             continue;
         p.dds_set_2bytes<false>(i, addr, (addr + type) | (addr << 8));
     }
+    while (!p.is_finished()) {
+    }
+    p.dump_dds(std::cout, i);
+}
+
+void output_dds(Molecube::Pulser &p, int i, int type)
+{
+    p.dump_dds(std::cout, i);
+    p.template dds_set_freq<false>(chn, freq2num(10e6 * type));
+    p.template dds_set_amp<false>(chn, amp2num(0.1 * type));
     while (!p.is_finished()) {
     }
     p.dump_dds(std::cout, i);
@@ -48,6 +85,12 @@ int main(int argc, char **argv)
     }
     else if (strcmp(argv[1], "write2") == 0) {
         write_dds(p, atoi(argv[2]), 2);
+    }
+    else if (strcmp(argv[1], "output1") == 0) {
+        output_dds(p, atoi(argv[2]), 1);
+    }
+    else if (strcmp(argv[1], "output2") == 0) {
+        output_dds(p, atoi(argv[2]), 2);
     }
 
     return 0;
